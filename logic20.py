754d58325e1c780ab83d8921374fb9ad930d728a
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2023-12-16 18:10:08 +0500

    20 tayyor

--- logic20.py
+++ logic20.py
@@ -7,6 +7,13 @@ def main(n):
         n(int): parameter n
     Returns:
         bool: answer
-    """
+    """ 
+    x1=n%10
+    x2=n//10%10
+    x3=n//100%10
+    x4=n//1000%10
+    x5=n//10000
+    return x1+x2+x3+x4+x5>=3
+print(main(10010))
 
-    return
+    
